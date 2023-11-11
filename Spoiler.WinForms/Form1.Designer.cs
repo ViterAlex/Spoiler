adb40c0294a3c69174f7db1529cf4d8fc1e0d41a
Oleksandr Viter <viter.alex@gmail.com>
2023-11-11 08:30:46 +0200

    Add project files.

---
+++ Spoiler.WinForms/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Spoiler.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.spoiler2 = new Spoiler.Lib.Spoiler();
+            this.button1 = new System.Windows.Forms.Button();
+            this.spoiler2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(435, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.propertyGrid1.HelpVisible = false;
+            this.propertyGrid1.Location = new System.Drawing.Point(516, 0);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.SelectedObject = this.spoiler2;
+            this.propertyGrid1.Size = new System.Drawing.Size(284, 328);
+            this.propertyGrid1.TabIndex = 2;
+            this.propertyGrid1.ToolbarVisible = false;
+            // 
+            // spoiler2
+            // 
+            this.spoiler2.Controls.Add(this.button1);
+            this.spoiler2.Location = new System.Drawing.Point(42, 62);
+            this.spoiler2.Name = "spoiler2";
+            this.spoiler2.Size = new System.Drawing.Size(394, 117);
+            this.spoiler2.TabIndex = 3;
+            this.spoiler2.Text = "Spoiler";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 328);
+            this.Controls.Add(this.spoiler2);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.spoiler2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private Lib.Spoiler spoiler2;
+    }
+}
+
---
+++ Spoiler.WinForms/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spoiler.WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            spoiler2.Text = "OK!";
+        }
+
+        //protected override void OnMouseClick(MouseEventArgs e)
+        //{
+        //    base.OnMouseClick(e);
+        //    spoiler2.Text = $"{e.Location}";
+        //}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //spoiler1.ClientSize = new Size(spoiler1.Width, SystemInformation.CaptionHeight);
+        }
+    }
+}
